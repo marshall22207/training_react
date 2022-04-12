bd93a4a68fca6e1d9e1f55e1b2f5b20835559e66
Thomas Kwak <Thomas@Thomass-MacBook-Pro.local>
2022-04-12 21:52:09 +0900

    lecture 9

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-import express from 'express'
+const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
 
-connect('mongodb+srv://vmware:vmware@cluster0.lyins.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err))
 
@@ -10,6 +21,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/register', (req, res) => {
+  // 회원 가입 정보
+  const user = new User(req.body)
+  user.save((err, userinfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
